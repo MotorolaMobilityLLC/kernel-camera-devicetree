929b298008727ebf9d3c03343b1e6b9f57a6781c
xizx <xizx@motorola.com>
2021-05-15 18:18:01 +0800

    camera-dts: Add Moto hiphi camera sensor dts base
    Base on waipio-camera-sensor-mtp.dts, clone moto hiphi product camera
    device tree overlay as base.
    
    waipio-camera-sensor-hiphi-common-base.dtsi is cloned from waipio-camera-sensor-mtp.dtsi
    without change.
    
    Change-Id: I9b6ba83680dd0e6c0001f2169fc6a68a04e1912a
    Reviewed-on: https://gerrit.mot.com/1949919
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- config/waipio.mk
+++ config/waipio.mk
@@ -1,8 +1,14 @@
 dtbo-$(CONFIG_ARCH_WAIPIO) := waipio-camera.dtbo
 dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-overlay-v2.dtbo \
-								waipio-camera-sensor-mtp.dtbo \
+#remove useless qcom device tree in moto build
+ifneq ($(CONFIG_MMI_DEVICE_DTBS),y)
+dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-sensor-mtp.dtbo \
 								waipio-camera-sensor-cdp.dtbo \
 								waipio-camera-sensor-qrd.dtbo
+else
+dtbo-$(CONFIG_MMI_DEVICE_DTBS) += \
+       waipio-camera-sensor-hiphi-evb1.dtbo
+endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
 
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera.dtbo
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-idp.dtbo
