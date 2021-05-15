614a732f9a44c11f53bfd6288562256fddc8a1f1
xizx <xizx@motorola.com>
2021-05-15 18:18:01 +0800

    camera-dts: Add Moto rtwo camera sensor dts base
    Add Moto camera sensoroverlay base, which data are all same with
    pure qcom kalama-camera-sensor-mtp.dts
    
    kalama-camera-sensor-rtwo-evb1.dts    : is derivated from kalama-camera-sensor-mtp.dts
    kalama-camera-sensor-rtwo-common.dtsi : is derivated from kalama-camera-sensor-mtp.dtsi
    
    Change-Id: I9b6ba83680dd0e6c0001f2169fc6a68a04e1912a
    Reviewed-on: https://gerrit.mot.com/1949919
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2241491
    Submit-Approved: Zhenxin Xi <xizx@motorola.com>

--- config/kalama.mk
+++ config/kalama.mk
@@ -1,4 +1,6 @@
 dtbo-$(CONFIG_ARCH_KALAMA) := kalama-camera.dtbo
+#remove useless qcom device tree in moto build
+ifneq ($(CONFIG_MMI_DEVICE_DTBS),y)
 dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 								kalama-camera-sensor-mtp.dtbo \
 								kalama-camera-sensor-qrd.dtbo \
@@ -9,3 +11,7 @@ dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
 				crow-camera-sensor-idp.dtbo \
 				crow-camera-sensor-qrd.dtbo
+endif
+
+#Moto camera sensor dtbo
+dtbo-$(CONFIG_RTWO_DTB) += kalama-camera-sensor-rtwo-evb1.dtbo
