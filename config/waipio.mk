b781620b579f0efb9f7c79d471740acf45516509
xizx <xizx@motorola.com>
2021-06-29 11:13:15 +0800

    hiphic: add new camera evb1 and evt1 dt overlay
    Support evb1 and evt1 for hiphic that is PRC sku
    
    changes:
    1. Camera does have a change in hiphi prc EVT1
    A sync pin has been added between all the rear camera modules and
    the GPIO 3 on the Camera PMIC on EVT1.
    The change to program the GPIO3 is handled in the PMIC OTP itself or we
    need to do that explicitly in SW in the PMIC driver. So need to change
    the PMIC driver then we will need to have a evt1specific camera dts
    
    Change-Id: Ic1bffc666e812c94897d163453c2ea86fb289def
    Signed-off-by: xizx <xizx@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1995475
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- config/waipio.mk
+++ config/waipio.mk
@@ -7,5 +7,7 @@ dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-sensor-mtp.dtbo \
 								waipio-camera-sensor-qrd.dtbo
 else
 dtbo-$(CONFIG_MMI_DEVICE_DTBS) += \
-       waipio-camera-sensor-hiphi-evb1.dtbo
+	waipio-camera-sensor-hiphi-evb1.dtbo \
+	waipio-camera-sensor-hiphic-evb1.dtbo \
+	waipio-camera-sensor-hiphic-evt1.dtbo
 endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
