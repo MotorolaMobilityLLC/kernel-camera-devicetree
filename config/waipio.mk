d1022afc5e803d065019ac43d7c9a884eecce622
Ravi Vembu <raviv@motorola.com>
2021-08-10 23:11:39 -0500

    arm/dt: hiphi: Support ECO sensor for front camera.
    ECO sensor is supported on hiphiC DVT1 and
    hiphi EVT and beyond. Split the device tree to support
    both ECO and non-ECO sensors with a different slot id
    for the front camera.
    ECO - slot1,  non-ECO - slot4.
    
    Change-Id: If6f67f4c5d39feabcab2840edef10466da16aea4
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Mot-CRs-Fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/2038568
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jian Luan <jamesluan@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>

--- config/waipio.mk
+++ config/waipio.mk
@@ -8,8 +8,10 @@ dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-sensor-mtp.dtbo \
 else
 dtbo-$(CONFIG_MMI_DEVICE_DTBS) += \
 	waipio-camera-sensor-hiphi-evb1.dtbo \
+	waipio-camera-sensor-hiphi-evt1.dtbo \
 	waipio-camera-sensor-hiphic-evb1.dtbo \
-	waipio-camera-sensor-hiphic-evt1.dtbo
+	waipio-camera-sensor-hiphic-evt1.dtbo \
+	waipio-camera-sensor-hiphic-dvt1.dtbo
 endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
 
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera.dtbo
