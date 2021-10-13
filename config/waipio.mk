c9f4d31307443ea09c6f5a55848a2ba62e785c72
wangxs8 <wangxs8@lenovo.com>
2021-10-13 16:52:52 +0800

    li: Add camera evb1 device tree
    Add Moto Li camera sensor evb1 device tree.
    
    Change-Id: I4608dc74fdca849f8a41a8363037023a18007c49
    Signed-off-by: wangxs8 <wangxs8@mt.com>
    Reviewed-on: https://gerrit.mot.com/2088809
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- config/waipio.mk
+++ config/waipio.mk
@@ -6,10 +6,16 @@ dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-sensor-mtp.dtbo \
 								waipio-camera-sensor-cdp.dtbo \
 								waipio-camera-sensor-qrd.dtbo
 else
+# Li camera bringup
+ifeq ($(CONFIG_LI_DTB),y)
+dtbo-$(CONFIG_MMI_DEVICE_DTBS) += \
+	waipio-camera-sensor-li-evb1.dtbo
+else
 dtbo-$(CONFIG_MMI_DEVICE_DTBS) += \
 	waipio-camera-sensor-hiphi-evb1.dtbo \
 	waipio-camera-sensor-hiphi-evt1.dtbo \
 	waipio-camera-sensor-hiphic-evb1.dtbo \
 	waipio-camera-sensor-hiphic-evt1.dtbo \
 	waipio-camera-sensor-hiphic-dvt1.dtbo
+endif  #($(CONFIG_LI_DTB),y)
 endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
