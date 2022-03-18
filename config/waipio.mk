0740f05c792df97d7c69b589bc021ebc2a64b8e2
chenzc2 <chenzc2@lenovo.com>
2022-03-18 17:50:26 +0800

    arm64/dt: oneli: add camera dtsi
    -add oneli dtsi in waipio.mk.
    -add oneli evt1.dts copied from li.
    -add oneli common.dtsi copied from li.
    
    Change-Id: I19b89cb61d0856ad90a23efe06953e0fa48d92bc
    Signed-off-by: chenzc2 <chenzc2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2220262
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- config/waipio.mk
+++ config/waipio.mk
@@ -41,4 +41,8 @@ ifeq ($(CONFIG_EQS_DTB),y)
 dtbo-$(CONFIG_ARCH_CAPE) += \
 	cape-camera-sensor-eqs-evt1.dtbo
 endif  #($(CONFIG_EQS_DTB),y)
+ifeq ($(CONFIG_ONELI_DTB),y)
+dtbo-$(CONFIG_ARCH_CAPE) += \
+	cape-camera-sensor-oneli-evt1.dtbo
+endif  #($(CONFIG_ONELI_DTB),y)
 endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
