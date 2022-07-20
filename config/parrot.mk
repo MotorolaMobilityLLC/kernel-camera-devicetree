47636a8a12207dae05b9d60496622627ef0910bc
chenzc2 <chenzc2@lenovo.com>
2022-07-20 09:19:35 +0800

    geneva: initialize camera dts
    Detail:
    a.Add geneva evb dts file.
    b.Add geneva common dtsi files.
    c.Add parrot makefile configuration for geneva.
    
    Change-Id: I7be4a090a8e0fca28dc7b9b4135bb5200f609a9f
    Signed-off-by: chenzc2 <chenzc2@mt.com>
    Reviewed-on: https://gerrit.mot.com/2338870
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Yun Jiang <jiangy3@motorola.com>
    Reviewed-by: Suna Li <lisn17@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- config/parrot.mk
+++ config/parrot.mk
@@ -1,3 +1,10 @@
 dtbo-$(CONFIG_ARCH_PARROT) := parrot-camera.dtbo
+#remove useless qcom device tree in moto build
+ifneq ($(CONFIG_MMI_DEVICE_DTBS),y)
 dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-idp.dtbo
 dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-qrd.dtbo
+else
+ifeq ($(CONFIG_GENEVA_DTB),y)
+dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-geneva-evb.dtbo
+endif  #($(CONFIG_GENEVA_DTB),y)
+endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
