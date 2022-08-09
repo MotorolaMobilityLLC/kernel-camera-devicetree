7afa6fc4d55d4b61707ca24afe7e7b920b530c6c
Dan Vacura <w36195@motorola.com>
2022-08-09 15:25:29 -0500

    felix: hw rev id updates
    Felix camera was failing when a hwrev was set, as the newer HW id's
    weren't being matched in the kernel probe for camera.
    
    Change-Id: Ia9ec90cb51de61edc5fb8547bd3eecf4accb37cb
    Reviewed-on: https://gerrit.mot.com/2358997
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jeffrey Vanhoof <jvanhoof@motorola.com>
    Reviewed-by: Rav(CR) Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- config/waipio.mk
+++ config/waipio.mk
@@ -58,6 +58,6 @@ dtbo-$(CONFIG_ARCH_CAPE) += \
 endif  #($(CONFIG_ZEEKR_DTB),y)
 ifeq ($(CONFIG_FELIX_DTB),y)
 dtbo-$(CONFIG_ARCH_CAPE) += \
-	cape-camera-sensor-felix-mortable.dtbo
+	cape-camera-sensor-felix-evt1.dtbo
 endif  #($(CONFIG_FELIX_DTB),y)
 endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
