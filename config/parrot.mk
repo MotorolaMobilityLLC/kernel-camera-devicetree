2cc986601addda6da80596d9d1d459cc105a65e4
shihl5@motorola.com <shihl5@motorola.com>
2022-08-25 20:04:45 +0800

    genevn: initialize camera dts
    Detail:
    a.Add genevn evb dts file.
    b.Add genevn common dtsi files.
    c.Add parrot makefile configuration for genevn.
    
    Change-Id: If5ace1363e8aae9cb43da54a532c9e6358be49ff
    Signed-off-by: shihl5@motorola.com <shihl5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2371245
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Suna Li <lisn17@motorola.com>
    Reviewed-by: <wangdp11@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/parrot.mk
+++ config/parrot.mk
@@ -7,4 +7,7 @@ else
 ifeq ($(CONFIG_GENEVA_DTB),y)
 dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-geneva-evb.dtbo
 endif  #($(CONFIG_GENEVA_DTB),y)
+ifeq ($(CONFIG_GENEVN_DTB),y)
+dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-genevn-evb.dtbo
+endif  #($(CONFIG_GENEVN_DTB),y)
 endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
