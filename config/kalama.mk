861112b7f9eda3ac9711adbd0f1dfe358e151862
Ayush Kumar <ayushkr@qti.qualcomm.com>
2023-02-25 20:18:32 +0530

    ARM: dts: msm: Add camera device tree support for Crow
    Add ICP, OPE, BPS, CDM, SMMU, JPEG, TFE, CSID and CPAS
    nodes for Crow camera.
    
    CRs-Fixed: 3426117
    Change-Id: I242c4fa7d815e770c5956ba7248b80b0b50aa7dd

--- config/kalama.mk
+++ config/kalama.mk
@@ -5,3 +5,5 @@ dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 								kalama-camera-sensor-hdk.dtbo \
 								kalama-sg-hhg-camera.dtbo \
 								kalama-sg-hhg-camera-sensor.dtbo
+
+dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo
