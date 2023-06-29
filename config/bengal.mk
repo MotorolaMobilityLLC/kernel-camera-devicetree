498499aed4dc9ccdf2432a6579a5a1db3f8ea556
Simon C <chenzm8@motorola.com>
2023-06-29 20:27:25 +0800

    fogona: Setup camera driver baseline[2/2]
    Details:
      Add camera devicetree configuration
    
    Change-Id: I7feed1707a859373fda38363766b1357a7286f9e
    Signed-off-by: Simon C <chenzm8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2660516
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <shihl5@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/bengal.mk
+++ config/bengal.mk
@@ -10,3 +10,8 @@ dtbo-$(CONFIG_ARCH_KHAJE) += khajeg-camera-idp.dtbo \
 				khaje-camera-sensor-qrd-nowcd9375.dtbo \
 				khaje-camera-sensor-idps-display-90hz.dtbo \
 				khaje-camera-sensor-idp-usbc.dtbo \
+
+ifeq ($(CONFIG_FOGONA_DTB),y)
+dtbo-$(CONFIG_ARCH_KHAJE) += \
+	khaje-camera-sensor-fogona-evb.dtbo
+endif  #($(CONFIG_FOGONA_DTB),y)
