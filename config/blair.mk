0bf36422172a38cae5304216855baa1480f49206
Rajat Goel <rajgoe@qti.qualcomm.com>
2023-07-25 13:41:19 +0530

    ARM: dts: msm: Initial Sensor changes for the devicetree compilation
    This change includes
    -base devicetree changes for blair as per kernel 6.1.
    -adds setu change to enable device-tree compilation.
    -adds all required sensor and its submodule nodes.
    
    CRs-Fixed: 3569444
    Change-Id: I51764b4cbe25b85f9e99847b8b322205f450d77d

--- config/blair.mk
+++ config/blair.mk
@@ -1,1 +1,4 @@
 dtbo-$(CONFIG_ARCH_BLAIR)	:= blair-camera.dtbo
+dtbo-$(CONFIG_ARCH_BLAIR)	+= blair-camera-sensor-mtp.dtbo
+dtbo-$(CONFIG_ARCH_BLAIR)	+= blair-camera-sensor-qrd.dtbo
+dtbo-$(CONFIG_ARCH_BLAIR)	+= blair-camera-sensor-cdp.dtbo
