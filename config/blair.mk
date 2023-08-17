ae38dcb72c777a7add1e8df5ac3b53b5ee6d013e
Rajat Goel <rajgoe@qti.qualcomm.com>
2023-08-17 11:15:05 +0530

    ARM: dts: msm: Base device tree changes for holi 2.5 on kernel 6.1
    This change includes
    -base devicetree changes for holi 2.5 as per kernel 6.1.
    -adds setup change to enable device-tree compilation.
    
    CRs-Fixed: 3619425
    Change-Id: I524f4da6d48350ed16f1f9e363cf6b4f2385695f
    (cherry picked from commit 0b2b8c8e35952141c3258f24564e4a422380f698)

--- config/blair.mk
+++ config/blair.mk
@@ -4,3 +4,6 @@ dtbo-$(CONFIG_ARCH_BLAIR)	+= blair-camera-sensor-qrd.dtbo
 dtbo-$(CONFIG_ARCH_BLAIR)	+= blair-camera-sensor-cdp.dtbo
 
 dtbo-$(CONFIG_ARCH_HOLI)        += holi-camera.dtbo
+dtbo-$(CONFIG_ARCH_HOLI)        += holi-camera-sensor-mtp.dtbo
+dtbo-$(CONFIG_ARCH_HOLI)        += holi-camera-sensor-qrd.dtbo
+dtbo-$(CONFIG_ARCH_HOLI)        += holi-camera-sensor-cdp.dtbo
