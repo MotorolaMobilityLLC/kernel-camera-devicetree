ef483a268a5295a4b1fccedc2b7b8824ab18f1d2
Yash Upadhyay <yupadhya@qti.qualcomm.com>
2023-09-13 08:48:05 +0530

    ARM: dts: msm: Base device tree changes for holi on kernel 6.1
    This change includes,base devicetree changes for holi as per kernel
    6.1. Adds setup change to enable device-tree compilation. Adds
    interconnect and removes default enabled clock from cpas node.
    
    CRs-Fixed: 3620229
    Change-Id: Ic3d5eb4f810a5c7fc8e7069afa2f5ecaa6de4358

--- config/blair.mk
+++ config/blair.mk
@@ -2,3 +2,5 @@ dtbo-$(CONFIG_ARCH_BLAIR)	:= blair-camera.dtbo
 dtbo-$(CONFIG_ARCH_BLAIR)	+= blair-camera-sensor-mtp.dtbo
 dtbo-$(CONFIG_ARCH_BLAIR)	+= blair-camera-sensor-qrd.dtbo
 dtbo-$(CONFIG_ARCH_BLAIR)	+= blair-camera-sensor-cdp.dtbo
+
+dtbo-$(CONFIG_ARCH_HOLI)        += holi-camera.dtbo
