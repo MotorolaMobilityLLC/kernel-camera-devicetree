0036d55bfa46b686524984397b066f23d9b13c0b
Alok Chauhan <alokc@qti.qualcomm.com>
2024-02-28 11:31:51 +0530

    ARM: dts: msm: Add camera device tree support for Volcano
    Add ICP, OPE, BPS, CDM, SMMU, TFE, CSID and CPAS
    nodes for Volcano camera.
    
    CRs-Fixed: 3746224
    Change-Id: Ie4fae67745498a18c8096b40779c78a8eefdc57b

--- config/pineapple.mk
+++ config/pineapple.mk
@@ -9,3 +9,4 @@ dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
 								cliffs-camera-sensor-mtp.dtbo \
 								cliffs-camera-sensor-qrd.dtbo
+dtbo-$(CONFIG_ARCH_VOLCANO)     += volcano-camera.dtbo
