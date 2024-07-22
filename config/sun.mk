8d15fbaa59bd96af9bf248568bb74fa8d3ac25e3
qianlong <qianl2@motorola.com>
2024-07-22 10:40:05 +0800

    camera-dts: Add camera sensor dts base for leap
    Add Moto camera sensoroverlay base, which data are all same with
    pure qcom sun-camera-sensor-mtp.dtsi, board-id use evb.
    
    sun-camera-sensor-leap-evt1a.dts   : from sun-camera-sensor-qrd.dts
    sun-camera-sensor-leap-common.dtsi : from sun-camera-sensor-qrd.dtsi
    
    Change-Id: Ia30895476f50dd9d91e466e5e92cfb54798ed714
    Signed-off-by: qianlong <qianl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3036237
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- config/sun.mk
+++ config/sun.mk
@@ -1,3 +1,10 @@
+ifeq ($(CONFIG_MMI_DEVICE_DTBS),y)
+dtbo-$(CONFIG_ARCH_SUN)  := sun-camera.dtbo
+
+dtbo-$(CONFIG_LEAP_DTB) += sun-camera-sensor-leap-evb.dtbo
+
+else
+
 dtbo-$(CONFIG_ARCH_SUN)   := sun-camera.dtbo
 dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
 				sun-camera-sensor-rumi.dtbo \
@@ -9,3 +16,4 @@ dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera.dtbo
 dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera-sensor-mtp.dtbo \
 				tuna-camera-sensor-cdp.dtbo \
 				tuna-camera-sensor-qrd.dtbo
+endif # end of CONFIG_MMI_DEVICE_DTBS
