e9cef989991972cca02dc0c09de032cc7e53a242
Pengfei Liu <quic_penliu@quicinc.com>
2024-12-04 10:44:14 -0800

    ARM: dts: msm: hdk rotate 180 degree for rear camera
    - HDK roll 180 degree compare to mtp/qrd.
    - Change dtsi roll degree of hdk.
    
    CRs-Fixed: 3979998
    Change-Id: I2f067dcf135443a04881257f840bf2ee87298473
    Signed-off-by: Pengfei Liu <quic_penliu@quicinc.com>

--- config/sun.mk
+++ config/sun.mk
@@ -2,4 +2,5 @@ dtbo-$(CONFIG_ARCH_SUN)   := sun-camera.dtbo
 dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
 				sun-camera-sensor-rumi.dtbo \
 				sun-camera-sensor-cdp.dtbo  \
+				sun-camera-sensor-hdk.dtbo  \
 				sun-camera-sensor-qrd.dtbo
