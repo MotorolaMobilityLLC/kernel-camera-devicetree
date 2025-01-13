d9017be0a9271d1c0794988bacc16c943c7cd801
Lc_Lg <longc168@motorola.com>
2025-01-13 11:50:45 +0800

    noida: init all camera dts
    details:
           add dts for all camera
    
    Change-Id: I2b578a9d62771c7902fa0fc12b5962b37fc22463
    Signed-off-by: Lc_Lg <longc168@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3200511
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <shihl5@motorola.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/parrot.mk
+++ config/parrot.mk
@@ -24,5 +24,6 @@ endif  #($(CONFIG_BOSTON_DTB),y)
 ifeq ($(CONFIG_CUSCO_DTB),y)
 dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-cusco-evb.dtbo
 dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-cuscoi-evb.dtbo
+dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-cuscoi-noi-evb.dtbo
 endif  #($(CONFIG_CUSCO_DTB),y)
 endif  #($(CONFIG_MMI_DEVICE_DTBS),y)
