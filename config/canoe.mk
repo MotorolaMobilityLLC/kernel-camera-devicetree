5f72b7609689241911fda7dc2ca2ee155c457296
Karthik Dillibabu <quic_kard@quicinc.com>
2025-03-20 10:51:25 -0700

    ARM: dts: msm: Add support for KaM
    Added soc id and dtsi changes to support Kaanapali
    on Molokai (KaM).
    
    CRs-Fixed: 4061137
    Change-Id: I094fc1fcd27826e4769c8fdaf2a4c86634f2eb51
    Signed-off-by: Karthik Dillibabu <quic_kard@quicinc.com>

--- config/canoe.mk
+++ config/canoe.mk
@@ -1,4 +1,5 @@
-dtbo-$(CONFIG_ARCH_CANOE)   := canoe-camera.dtbo
+dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera.dtbo \
+	alor-interposer-camera.dtbo
 dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-sensor-mtp.dtbo \
 	canoe-camera-sensor-cdp.dtbo \
 	canoe-camera-sensor-qrd.dtbo \
