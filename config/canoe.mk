5a13080e323c0bd9115ca3c5d3f00b5b97d1a3ef
Mukund Madhusudan Atre <quic_matre@quicinc.com>
2025-04-07 10:51:07 -0700

    ARM: dts: msm: Add v2 camera device tree support for canoe
    Add camera v2 device tree support for canoe. Remove
    workaround for hw errata in v1 for tfe clocks. Remove
    msm ids belonging to v2 from the v1 dts.
    
    CRs-Fixed: 4091569
    Change-Id: I087cf7c1edfb034bc88f3b2f085c084bcc6bd8af
    Signed-off-by: Mukund Madhusudan Atre <quic_matre@quicinc.com>

--- config/canoe.mk
+++ config/canoe.mk
@@ -2,7 +2,8 @@ dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera.dtbo \
 	alor-interposer-camera.dtbo \
 	alor-interposer-camera-sensor-cdp.dtbo \
 	alor-interposer-camera-sensor-mtp.dtbo
-dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-sensor-mtp.dtbo \
+dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-v2.dtbo \
+	canoe-camera-sensor-mtp.dtbo \
 	canoe-camera-sensor-cdp.dtbo \
 	canoe-camera-sensor-qrd.dtbo \
 	canoe-camera-sensor-rumi.dtbo
