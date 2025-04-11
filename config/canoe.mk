3109cc5c3a97ef40f572fdb8b8e505d62aeeb765
Shravan Nevatia <quic_snevatia@quicinc.com>
2025-04-11 09:51:17 -0700

    ARM: dts: msm: Add camera support for KaM QRD
    Add camera sensor DT overlay for Alor interposer QRD platform.
    
    Change-Id: I6b273d3546e8bb33ded459c68b81efb38b4ee032
    CRs-Fixed: 4106383
    Signed-off-by: Shravan Nevatia <quic_snevatia@quicinc.com>

--- config/canoe.mk
+++ config/canoe.mk
@@ -1,7 +1,8 @@
 dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera.dtbo \
 	alor-interposer-camera.dtbo \
 	alor-interposer-camera-sensor-cdp.dtbo \
-	alor-interposer-camera-sensor-mtp.dtbo
+	alor-interposer-camera-sensor-mtp.dtbo \
+	alor-interposer-camera-sensor-qrd.dtbo
 dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-v2.dtbo \
 	canoe-camera-sensor-mtp.dtbo \
 	canoe-camera-sensor-cdp.dtbo \
