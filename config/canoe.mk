443b4e33a59122692f007f9ae39f48a973e76239
Pengfei Liu <quic_penliu@quicinc.com>
2025-05-28 10:51:08 -0700

    ARM: dts: msm: Add canoe hdk sensor devicetree changes
    Add support for canoe hdk sensor device config updates
    for multi camera subdevices.
    
    CRs-Fixed: 4153524
    Change-Id: I75c15b6cb909756117dc7e7fafdbf72078042015
    Signed-off-by: Liu Pengfei <penliu@qti.qualcomm.com>

--- config/canoe.mk
+++ config/canoe.mk
@@ -9,4 +9,5 @@ dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-v2.dtbo \
 	canoe-camera-sensor-mtp.dtbo \
 	canoe-camera-sensor-cdp.dtbo \
 	canoe-camera-sensor-qrd.dtbo \
+	canoe-camera-sensor-hdk.dtbo \
 	canoe-camera-sensor-rumi.dtbo
