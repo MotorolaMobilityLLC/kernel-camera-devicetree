7491281b1678e6aafd4ebe25aafb58d815c97d09
Mukund Deshmukh <mdeshmuk@qti.qualcomm.com>
2025-06-09 11:21:14 -0700

    ARM: dts: msm: Initial camera sensor dtsi files for Kera
    Initial sensor dtsi files for Kera MTP/CDP/RCM/QRD with:
    - Peripherals: CCI0/CCI1,
    - CSIPHY instances: 0 to 4,
    - Sensors: IMX766/IMX858/JN1/IMX688 nodes,
    - TPG instances: 1 to 3,
    - MCLK/RESET GPIO pin controls
    
    CRs-Fixed: 3970958.
    Change-Id: Ieb49cd277e1e3eb7b48344c6098e82b28dee2b20
    Signed-off-by: Mukund Deshmukh <mdeshmuk@qti.qualcomm.com>
    (cherry picked from commit 02bf75d48c03e898dfd27f01953912ae5d78369a)

--- config/sun.mk
+++ config/sun.mk
@@ -7,4 +7,12 @@ dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
 
 dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera.dtbo
 
+dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera-sensor-mtp.dtbo \
+				tuna-camera-sensor-cdp.dtbo \
+				tuna-camera-sensor-qrd.dtbo
+
 dtbo-$(CONFIG_ARCH_KERA)  += kera-camera.dtbo
+dtbo-$(CONFIG_ARCH_KERA)  += kera-camera-sensor-mtp.dtbo \
+				kera-camera-sensor-cdp.dtbo \
+				kera-camera-sensor-qrd.dtbo \
+				kera-camera-sensor-rcm.dtbo
