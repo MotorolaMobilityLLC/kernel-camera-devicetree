bd40520e38818a8a9e01220a5a77b5fc52060d35
Shravan Nevatia <snevatia@qti.qualcomm.com>
2025-06-09 11:21:17 -0700

    ARM: dts: msm: Add alor overlays to makefile
    Add the camera sensor overlays for alor platforms to
    the makefile.
    
    CRs-Fixed: 4124744
    Change-Id: Iaca13cfa86b703c75d4004ff68f0e32be001063f
    Signed-off-by: Shravan Nevatia <snevatia@qti.qualcomm.com>

--- config/canoe.mk
+++ config/canoe.mk
@@ -10,4 +10,8 @@ dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-v2.dtbo \
 	canoe-camera-sensor-cdp.dtbo \
 	canoe-camera-sensor-qrd.dtbo \
 	canoe-camera-sensor-hdk.dtbo \
-	canoe-camera-sensor-rumi.dtbo
+	canoe-camera-sensor-rumi.dtbo \
+	alor-camera-sensor-mtp.dtbo \
+	alor-camera-sensor-cdp.dtbo \
+	alor-camera-sensor-mtp-peach.dtbo \
+	alor-camera-sensor-rcm-peach.dtbo
