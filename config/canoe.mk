30771f8c9e9d847b73a0b6ac1fb5ced74494db11
zhaocao <zhaocao@qti.qualcomm.com>
2025-06-09 11:21:19 -0700

    ARM: dts: msm: Camera sensor DT support for alor (QRD)
    Add camera sensor DT support for alor QRD platforms.
    
    CRs-Fixed: 4140637
    Change-Id: If1758108c94881de7143457b6f65cebf5d1ad95d
    Signed-off-by: zhaocao <zhaocao@qti.qualcomm.com>

--- config/canoe.mk
+++ config/canoe.mk
@@ -13,5 +13,6 @@ dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-v2.dtbo \
 	canoe-camera-sensor-rumi.dtbo \
 	alor-camera-sensor-mtp.dtbo \
 	alor-camera-sensor-cdp.dtbo \
+	alor-camera-sensor-qrd.dtbo \
 	alor-camera-sensor-mtp-peach.dtbo \
 	alor-camera-sensor-rcm-peach.dtbo
