2fbb49fd10d0f4ff6e8bb9fd6c3bf56f506b3dd5
qianlong <qianl2@motorola.com>
2025-06-10 15:36:24 +0800

    camera: Add vantg evt1 dts
    Camera hardware modify so need add evt1
    dts.
    
    Change-Id: I2634852410e05c2303126331bc383348e0651362
    Signed-off-by: qianlong <qianl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3424422
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/canoe.mk
+++ config/canoe.mk
@@ -3,7 +3,8 @@ CONFIG_YES=y
 ifeq ($(CONFIG_MMI_DEVICE_DTBS),$(CONFIG_YES))
 dtbo-$(CONFIG_ARCH_CANOE)  := canoe-camera.dtbo
 
-dtbo-$(CONFIG_VANTG_DTB) += canoe-camera-sensor-vantg-evb.dtbo
+dtbo-$(CONFIG_VANTG_DTB) += canoe-camera-sensor-vantg-evb.dtbo \
+                            canoe-camera-sensor-vantg-evt1.dtbo
 
 else
 
