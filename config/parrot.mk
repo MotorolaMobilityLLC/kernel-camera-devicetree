239c3c38eb4c5b0692a8f14f313643cbc89c96d1
Sanskar Pratap <sanskaro@qti.qualcomm.com>
2025-06-20 08:46:28 +0530

    ARM: dts: msm: Add soc-id support for Netrani-lite
    Add support for soc_id 715 for Netrani-lite.
    
    Change-Id: Ibbd90f3149d1749a5223b448046acb5d5af695f9
    (cherry picked from commit 5277b3a73444f62a3f90a1d3e586bd6889b14e95)

--- config/parrot.mk
+++ config/parrot.mk
@@ -1,4 +1,5 @@
 dtbo-$(CONFIG_ARCH_PARROT) := parrot-camera.dtbo
+dtbo-$(CONFIG_ARCH_PARROT) += parrot-lite-camera.dtbo
 dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-idp.dtbo
 dtbo-$(CONFIG_ARCH_PARROT) += parrot-camera-sensor-qrd.dtbo
 
